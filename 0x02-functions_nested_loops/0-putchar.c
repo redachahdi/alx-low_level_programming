153d50c2d2ce1d5d3b03dff2838cd8e5d09acda6
redachahdi <reda.elkhattabi01@gmail.com>
2023-03-21 08:42:08 -0700

    _putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main- entry point
+ * let print _putchar to stdout
+ * retun 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}
