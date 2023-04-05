96f69150d493a291f7711f490b832f0b34efc4ed
redachahdi <reda.elkhattabi01@gmail.com>
2023-04-05 08:39:19 -0700

    Why is it so important to dream task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -45,5 +45,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void print_diagsums(int *a, int size);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
 
