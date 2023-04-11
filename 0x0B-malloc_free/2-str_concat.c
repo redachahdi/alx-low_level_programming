7a7c357f8ec03a00b89fec55c5b3f1d631d79eef
redachahdi <reda.elkhattabi01@gmail.com>
2023-04-11 09:16:59 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - is include the two strings.
+ *
+ * @s1: is the first string.
+ * @s2: is the second string.
+ *
+ * Return: is the array's pointer of the chars.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *down;
+	unsigned int a, b, c, finish;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+
+	down = malloc(sizeof(char) * (a + b + 1));
+
+	if (down == NULL)
+	{
+		free(down);
+		return (NULL);
+	}
+
+	for (c = 0; c < a; c++)
+		down[c] = s1[c];
+
+	finish = c;
+	for (b = 0; b <= finish; c++, b++)
+		down[c] = s2[b];
+
+	return (down);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -55,5 +55,6 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
 
