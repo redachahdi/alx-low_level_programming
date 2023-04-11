0d4d5217943bdd71af5a32386fefce2350b29c98
redachahdi <reda.elkhattabi01@gmail.com>
2023-04-11 10:39:36 -0700

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - that includes the arguments of the program.
+ *
+ * @ac: is the argumentof the count.
+ *
+ * @av: is the argument of thrvector.
+ *
+ * Return: is the array's pointer of the char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *alpha;
+	int n, h, k, D;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (n = h = 0; h < ac; h++)
+	{
+		if (av[h] == NULL)
+			return (NULL);
+
+		for (k = 0; av[h][k] != '\0'; k++)
+			n++;
+		n++;
+	}
+
+	alpha = malloc((n + 1) * sizeof(char));
+
+	if (alpha == NULL)
+	{
+		free(alpha);
+		return (NULL);
+	}
+
+	for (h = k = D = 0; D < n; k++, D++)
+	{
+		if (av[h][k] == '\0')
+		{
+			alpha[D] = '\n';
+			h++;
+			D++;
+			k = 0;
+		}
+		if (n - 1 > D)
+			alpha[D] = av[h][k];
+	}
+	alpha[D] = '\0';
+
+	return (alpha);
+}
