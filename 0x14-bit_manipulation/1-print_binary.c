d6197ee6b8157ac94c2677f647ecc8e8cc44d2f5
redachahdi <reda.elkhattabi01@gmail.com>
2023-05-04 04:42:52 -0700

    task 0001

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - function that print the binary.
+ *
+ * @n : is the unsigned int
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+
+}
